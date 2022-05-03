77bcbc1217516300013254b602f93312ef2feb86
markjso <97271827+markjso@users.noreply.github.com>
2022-05-03 17:16:49 +0930

    Update ft_printf_ptr.c

--- ft_printf_ptr.c
+++ ft_printf_ptr.c
@@ -9,7 +9,13 @@
 /*   Updated: 2022/05/02 16:36:15 by jmarks           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-#include "printf.h"
+#include "ft_printf.h"
+
+/* This function uses the ft_printf_hex function for the format specifier p
+ * to print a pointer in hexadecimal format. We use ft_putstr_fd to write 
+ * "0x" as it is hexadecimal, and increment count by two. We then use our ft_print
+ * _hex function and use the lowercase hexadecimal as our base to print ptr. We then 
+ * return count to ft_printf.*/
 
 int	ft_printf_ptr(void *ptr)
 {
