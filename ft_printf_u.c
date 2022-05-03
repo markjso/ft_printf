48c3b1406e848819965aa6e9d42139d80597d7ee
markjso <97271827+markjso@users.noreply.github.com>
2022-05-03 17:36:44 +0930

    Update ft_printf_u.c

--- ft_printf_u.c
+++ ft_printf_u.c
@@ -10,7 +10,12 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "printf.h"
+#include "ft_printf.h"
+
+/* This function is used by format specifier u to print an unsigned decimal number.
+ * If num is greater than or equal to 10 we recursively call ft_printf_number until
+ * num becomes a single digit. We then add '0' to it to make it a character value and
+ * use ft_printf_char to write it. We then return the count to ft_printf*/
 
 int	ft_printf_u(unsigned int num)
 {
