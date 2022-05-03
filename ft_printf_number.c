15c2c44ca905a8782cbf216cb355b522df5ef0f3
markjso <97271827+markjso@users.noreply.github.com>
2022-05-03 16:59:48 +0930

    Update ft_printf_number.c

--- ft_printf_number.c
+++ ft_printf_number.c
@@ -10,8 +10,18 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "printf.h"
+#include "ft_printf.h"
 
+/* This function is used by the format specifiers i & d to print a signed 
+ * base 10 number. The first if checks if num is negative and if so writes a
+ * - using the ft_printf_char function, then declares n and gives it the value of
+ * num multiplied by -1 (making it positve).
+ *
+ * If num is not negative then n is equal to it (as an unsigned int). Then if n is greater 
+ * than or equal to 10 we recursively call ft_printf_number until n becomes a single digit. 
+ * We then add '0' to it to make it a character value and use ft_printf_char to write it. 
+ * We then return the count to ft_printf.
+ 
 int	ft_printf_number(int num)
 {
 	unsigned int	n;
