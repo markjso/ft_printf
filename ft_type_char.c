46e11ee33c240c07c75e15f607aa6f81dfef014a
markjso <97271827+markjso@users.noreply.github.com>
2022-05-04 11:19:05 +0930

    Update ft_type_char.c

--- ft_type_char.c
+++ ft_type_char.c
@@ -14,25 +14,25 @@
 
 /* This function is called when ft_printf finds a format specifier. It uses
  * the va_list pf to store the variables for each function. If c matches the format 
- * specifier in ft_printf, it uses the corresponding function to print and returns
- * the count to ft_printf*/
+ * specifier in ft_printf, it uses the corresponding function to print and return
+ * the number of characters to ft_printf*/
 
-int	ft_type_char(const char c, va_list pf)
+int	ft_type_char(const char c, va_list args)
 {
 	if (c == 'c')
-		return (ft_printf_char(va_arg(pf, int)));
+		return (ft_printf_char(va_arg(args, int)));
 	if (c == 's')
-		return (ft_printf_str(va_arg(pf, char *)));
+		return (ft_printf_str(va_arg(args, char *)));
 	if (c == 'i' || c == 'd')
-		return (ft_printf_number(va_arg(pf, int)));
+		return (ft_printf_number(va_arg(args, int)));
 	if (c == 'u')
-		return (ft_printf_u(va_arg(pf, int)));
+		return (ft_printf_u(va_arg(args, int)));
 	if (c == 'x')
-		return (ft_puthex(va_arg(pf, int), "0123456789abcdef"));
+		return (ft_puthex(va_arg(args, int), "0123456789abcdef"));
 	if (c == 'X')
-		return (ft_puthex(va_arg(pf, int), "0123456789ABCDEF"));
+		return (ft_puthex(va_arg(args, int), "0123456789ABCDEF"));
 	if (c == 'p')
-		return (ft_printf_ptr(va_arg(pf, void *)));
+		return (ft_printf_ptr(va_arg(args, void *)));
 	if (c == '%')
 		return (ft_printf_char('%'));
 	else
