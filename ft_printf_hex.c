b599ef92b386bb2f937ad93a2c0d1032a4101817
markjso <97271827+markjso@users.noreply.github.com>
2022-05-04 12:17:06 +0930

    Update ft_printf_hex.c

--- ft_printf_hex.c
+++ ft_printf_hex.c
@@ -18,7 +18,12 @@
  * (upper or lower case depending on base) of the % of sixteen at each call. 
  * We then return the count to ft_printf.
  *
- * Lastly we use the function ft_puthex to write the hexadecimal version of num.*/
+ * Lastly we use the function ft_puthex to write the hexadecimal version of num.
+ * NOTE - This is how the hex version of a number is calculated using recursion.
+ * 1221/16 = 76 with a remainder of 5. The hex value of 5 is 5.
+ * 76/16 = 4 with a remainder of 12. The hex value of 12 is C.
+ * 4/16 = 0 with a remainder of 4. The hex value of 4 is 4.
+ * The hex value of 1220 is therefore 4C5 (reversed because it is nested).
 
 int	ft_printf_hex(size_t num, char *base)
 {
